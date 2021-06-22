de140e017950d60720d4719250c422bb90675052
Alexey Nekora <gorkan86@gmail.com>
2021-06-22 21:04:52 +0300

    last changes

--- New Unity Project (1)/Assets/Scripts/Array.cs
+++ New Unity Project (1)/Assets/Scripts/Array.cs
@@ -7,19 +7,48 @@ public class Array : MonoBehaviour
     //[SerializeField] private int[] freeDays;  
     //private string[] summerMonths = new string[3] {"June", "July", "August" };
     //private string[] month = new string[12]; //massiv dla 
-
     private string[] films = new string[5] { "Mad Max 2", "Kill Bill", "Lion King", "WALLE", "Forsage 1-9" };
+    private string filmsList = "";
     void Start()
     {
         //print("SEICHAS " + summerMonths[0]);
         //print("Free days " + freeDays[0] + freeDays[1]);
-        for(int i = 0; i < 5; i++)
-        print(films[i]);
-    }
+        //for(int i = 0; i < films.Length; i++)
+        // print(films[i]);
 
-    
-    void Update()
-    {
-        
+        //foreach (string film in films)
+        //{
+        //    print(film);
+        //}
+        ////PrintArray(films);
+        foreach (string film in films)
+        {
+            filmsList = filmsList + " , " + film;
+            if (film == films[4])
+            {
+                filmsList = filmsList + "!";
+            }
+        }
+            print(filmsList);
+
+
+            //if ("WALLE" != film)
+            //{
+            //    print(film);
+
+            //}
+
+        }
+        //private void PrintArray(string[] array)
+        //{
+        //    foreach (string film in array)
+        //    {
+        //        print(film);
+        //    }
+        //}
+
+        void Update()
+        {
+
+        }
     }
-}
---
+++ New Unity Project (1)/Assets/Scripts/MyFunck.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFunck : MonoBehaviour
+{
+   
+    void Start()
+    {
+        //PrintWelcome();
+        //PrintWelcome();
+        //PrintWelcome();
+
+        //Divide(5f, 2f);
+        //Divide(5, 2);
+        //CheckAge(35, "Alexey");
+        CountSquare(60, 20);
+
+
+    }
+
+    private void CountSquare(float width, float height)
+    {
+        float rez = width * height / 100;
+        print(rez);
+    }
+
+
+
+    private void PrintWelcome()
+    {
+        print("Privet");
+        print("Dobro pozhalovat");
+    }
+
+    private void Divide(float num1, float num2)
+    {
+        float rez = num1 / num2;
+        print(rez);
+    }
+
+    private void Divide(int num1, int num2)
+    {
+        float rez = num1 / num2;
+        print(rez);
+    }
+
+    private void CheckAge(int yourAge, string yourName)
+    {
+        if (yourAge >= 18)
+        {
+            print("Dobro pozhalovat" + yourName);
+        }
+        
+        
+            else
+            { 
+            print("Zhdite" + yourName + (18-yourAge));
+            }
+        
+}
