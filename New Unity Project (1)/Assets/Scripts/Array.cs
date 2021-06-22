a3babed3381bd5b58237af6a54529c1c5be6c16f
Alexey Nekora <gorkan86@gmail.com>
2021-06-22 19:02:34 +0300

    New

--- New Unity Project (1)/Assets/Scripts/Array.cs
+++ New Unity Project (1)/Assets/Scripts/Array.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Array : MonoBehaviour
 {
-    [SerializeField] private int[] freeDays;  
-    private string[] summerMonths = new string[3] {"June", "July", "August" };
-    
+    //[SerializeField] private int[] freeDays;  
+    //private string[] summerMonths = new string[3] {"June", "July", "August" };
+    //private string[] month = new string[12]; //massiv dla 
+
+    private string[] films = new string[5] { "Mad Max 2", "Kill Bill", "Lion King", "WALLE", "Forsage 1-9" };
     void Start()
     {
-        print("SEICHAS " + summerMonths[0]);
-        print("Free days " + freeDays[0] + freeDays[1]);
+        //print("SEICHAS " + summerMonths[0]);
+        //print("Free days " + freeDays[0] + freeDays[1]);
+        for(int i = 0; i < 5; i++)
+        print(films[i]);
     }
 
     
