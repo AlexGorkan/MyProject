a003b1cd184c156d5b04248b2a658e711f541627
Alexey Nekora <gorkan86@gmail.com>
2021-06-29 07:49:33 +0300

    HW

--- New Unity Project (1)/Assets/Scripts/MyLists.cs
+++ New Unity Project (1)/Assets/Scripts/MyLists.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class MyLists : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> balls;
+    //[SerializeField] private List<GameObject> balls;
     [SerializeField] private List<GameObject> balls1 = new List<GameObject>();
+    [SerializeField] private Vector3 ballsStartPosition = new Vector3(0f, 0f, 0f);
+    [SerializeField] private float speed;
+    private int count = 0;
 
     void Start()
     {
@@ -14,8 +17,19 @@ public class MyLists : MonoBehaviour
         for (int i = 0; i < transform.childCount; i++)
         {
             balls1.Add(transform.GetChild(i).gameObject);
-        }
+            //transform.gameObject.transform.position = ballsStartPosition;
+            ballsStartPosition.x = 0;
+            gameObject.transform.Translate(Vector3.right);
+            if (gameObject.transform.position.y >= (ballsStartPosition.y + 2))
+            {
+                transform.position = Vector3.right;
+                transform.gameObject.transform.Translate(new Vector3(0f, ballsStartPosition.y, 0f));
+                count++;
+            }
+            
 
+        }
+        
 
     }
 
